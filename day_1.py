994007f732cfa4f344bc749eaccc7a419189380e
vanshchaudhary1122 <vanshlalera@gmail.com>
2026-02-16 08:55:01 +0530

    update

---
+++ day_1.py
@@ -0,0 +1,26 @@
+# ptint function 
+print("hello world ")
+print("my name is vansh chaudhary 'i am studying in B.Tech final year' ")
+# escape sequence 
+print("i am interestwd in machine learning\n also deep lerning")
+print("i am interestwd in machine learning\t also deep lerning")
+# escape sequence as normal text 
+print("i am interestwd in machine learning\\n also deep lerning")
+# python as calculator or operators in python 
+print(2+3)
+print(8-5)
+print(4/2) # float divison 
+print(4//2)# integer divison 
+print(2**3)# exponent right to left 
+print(3**3**2)
+print(8*9)
+# associativity rule 
+print((2+3)*4/2)
+# variable name 
+name = "vansh chaudhary "
+# naming rules for variable 
+# do not start from numeric value 
+# do not use any special character only use _
+# convention for variable naming 
+# use snake case writing
+my_name ="chaudhary "
---
+++ day_2.py
@@ -0,0 +1,31 @@
+# strings 
+name="vansh chaudhary "
+print(name)
+# string indexing 
+print(name[1])
+print(name[-1])
+# string alicing 
+print(name[0:5])
+print(name[0:-3])
+print(name[0:5:1])
+# take user input 
+age=input("enter your age:")
+print(age)
+# take 2 user input in a single line 
+user_name,age= input("enter your name and age:").split(",")
+print(user_name)
+print(age)
+# function in string 
+print(len(name))
+print(name.lower())
+print(name.upper())
+print(name.title())
+print(name.find("r"))
+print(name.find("r",2))
+name.center(11,"*")
+name.replace("h","H")
+# string is mutable 
+
+
+      
+
